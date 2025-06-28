8ee2d246257b86af7845b245d79ad4f78d9f6b9d
Harsh Sapariya <Harsh Sapariya@LAPTOP-DCSIFR0Q>
2025-06-28 11:40:55 +0530

    Added login test cases and report loggers

--- OrangeHRM/src/test/java/com/PageClass/LoginPage.java
+++ OrangeHRM/src/test/java/com/PageClass/LoginPage.java
@@ -23,6 +23,13 @@ public class LoginPage {
 
 	By dashboard_graph = By.xpath("//div[@class='emp-distrib-chart']/div/canvas");
 
+	By login_alert_Message = By.cssSelector(".oxd-text.oxd-text--p.oxd-alert-content-text");
+
+	By username_error_message = By.xpath("//input[@placeholder='Username']/../../span");
+	By password_error_message = By.xpath("//input[@placeholder='Password']/../../span");
+
+	By forgot_Password_Title = By.cssSelector(".oxd-text.oxd-text--h6.orangehrm-forgot-password-title");
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		log.info(this.getClass().getSimpleName() + " object initialized.");
@@ -57,10 +64,49 @@ public class LoginPage {
 		return driver.findElement(link_forget_fwd).isDisplayed();
 	}
 
+	public void clickOnForgetPWDLink() {
+
+		log.info("Clicking on Forget Password Link.");
+
+		driver.findElement(link_forget_fwd).click();
+	}
+
+	public String getForgetPasswordTitle() {
+		log.info("Verifying Forget Password Header Title.");
+
+		Utilities.waitForElement(driver.findElement(forgot_Password_Title));
+		return driver.findElement(forgot_Password_Title).getText();
+	}
+
 	public void dashboardIsDisplayed() {
 		Utilities.waitForElement(driver.findElement(dashboard_graph));
 		log.info("Dashboard graph is displayed.");
 
+	}
+
+	public String verifyLoginAlertMessage() {
+		Utilities.waitForElement(driver.findElement(login_alert_Message));
+
+		String alertMessage = driver.findElement(login_alert_Message).getText();
+
+		log.info("Verifying login alert message :" + alertMessage);
+
+		return alertMessage;
+
+	}
+
+	public String getUsernameErrorMessage() {
+
+		log.info("Verifying username error message");
+
+		return driver.findElement(username_error_message).getText();
+	}
+
+	public String getPasswordErrorMessage() {
+
+		log.info("Verifying password alert message ");
+
+		return driver.findElement(password_error_message).getText();
 	}
 
 }
--- OrangeHRM/src/test/java/com/Utilities/ListnerClass.java
+++ OrangeHRM/src/test/java/com/Utilities/ListnerClass.java
@@ -11,6 +11,8 @@ public class ListnerClass implements ITestListener {
 	@Override
 	public void onTestStart(ITestResult result) {
 		ReportManager.test = ReportManager.report.createTest(result.getMethod().getMethodName());
+		System.out.println("***** Test started for " + result.getName() + "*****");
+
 	}
 
 	@Override
@@ -30,11 +32,16 @@ public class ListnerClass implements ITestListener {
 	@Override
 	public void onStart(ITestContext context) {
 		ReportManager.setupExtendReport();
+
+		System.out.println("***** Test started for " + context.getName() + "*****");
 	}
 
 	@Override
 	public void onFinish(ITestContext context) {
 		ReportManager.flushReport();
+
+		System.out.println("***** Test finish for " + context.getName() + "*****");
+
 	}
 
 }
